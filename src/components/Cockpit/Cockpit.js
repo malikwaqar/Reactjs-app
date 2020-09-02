f094d8086c59e61215224e108c0c6756222eb05d
Malik Waqar Ul Hassan <malikwaqarofficial@gmail.com>
2020-09-03 01:39:34 +0500

    Passing Unknown Props

---
+++ src/components/Cockpit/Cockpit.js
@@ -0,0 +1,47 @@
+import React, { useEffect } from 'react';
+
+import classes from './Cockpit.css';
+
+const cockpit = ( props ) => {
+  useEffect(() => {
+    console.log('[Cockpit.js] useEffect');
+    // Http request...
+    setTimeout ( () => {
+      alert('Saved data to Cloud!');
+    } , 1000);
+    return () => {
+      console.log('[Cockpit.js] cleanup Work in UseEffect');
+    };
+  }, []);
+
+  useEffect ( () => {
+    console.log('[Cockpit.js] 2nd useEffect');
+    return () => {
+      console.log('[Cockpit.js] cleanup Work in UseEffect');
+    };
+  });
+    const assignedClasses = [];
+    let btnClass = '';
+    if (props.showPersons) {
+        btnClass = classes.Red;
+    }
+
+    if ( props.personsLength <= 2 ) {
+      assignedClasses.push( classes.red ); // classes = ['red']
+    }
+    if ( props.personsLength <= 1 ) {
+      assignedClasses.push( classes.bold ); // classes = ['red', 'bold']
+    }
+
+    return (
+        <div className={classes.Cockpit}>
+            <h1>{props.title}</h1>
+            <p className={assignedClasses.join( ' ' )}>This is really working!</p>
+            <button
+                className={btnClass}
+                onClick={props.clicked}>Toggle Persons</button>
+        </div>
+    );
+};
+
+export default React.memo(cockpit);
